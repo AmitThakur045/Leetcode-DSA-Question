4c60dfb2ae610c655b3f2cca185f49c3ad6bc991
Amit Kumar Thakur <84630298+AmitThakur045@users.noreply.github.com>
2021-08-13 13:03:12 +0530

    Create N-ary Tree Level Order Traversal.cpp

---
+++ Leetcode August/N-ary Tree Level Order Traversal.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if(root == NULL)
+            return {};
+        
+        vector<vector<int>>res;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+            for(int i=0; i<size; i++) {
+                Node* tmp = q.front();
+                q.pop();
+                level.push_back(tmp->val);
+                for(auto i : tmp->children) {
+                    q.push(i);
+                }
+            }
+            res.push_back(level);
+        }
+        return res;
+    }
+};
