37b4d97a696aa0a689ca0263836bd01eb0513dfb
Amit Kumar Thakur <84630298+AmitThakur045@users.noreply.github.com>
2021-08-20 13:52:31 +0530

    Create Valid Sudoku.cpp

---
+++ Leetcode August/Valid Sudoku.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9] = {0}, col[9][9] = {0}, box[9][9] = {0};
+        
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                if(board[i][j] != '.') {
+                    int k = ((i / 3) * 3) + (j / 3);    // to determine the the subboxes 
+                    int num = board[i][j] - '0' - 1;
+                    
+                    if(row[i][num] || col[j][num] || box[k][num])
+                        return false;
+                    row[i][num] = col[j][num] = box[k][num] = 1;
+                }
+            }
+        }
+        return true;
+    }
+};
