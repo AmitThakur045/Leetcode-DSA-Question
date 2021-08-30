bb01ea9cb5e0911cd9c3e39aa61d6c871f8a7920
Amit Kumar Thakur <84630298+AmitThakur045@users.noreply.github.com>
2021-08-30 14:41:37 +0530

    Create Range Addition II.cpp

---
+++ Leetcode August/Range Addition II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        for(int i=0; i<ops.size(); i++) {
+            if(m > ops[i][0])
+                m = ops[i][0];
+            if(n > ops[i][1])
+                n = ops[i][1];
+        }
+        return m*n;
+    }
+};
