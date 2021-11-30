b9a64c25934041d0284e7d7d27832fc8951ca1db
Amit Kumar Thakur <84630298+AmitThakur045@users.noreply.github.com>
2021-11-30 17:30:50 +0530

    Create Product of Array Except Self.cpp

---
+++ Leetcode November/Product of Array Except Self.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        
+        if(n < 1)
+            return ans;
+        
+        int prod = 1;
+        for(int i=0; i<n; i++) {
+            prod *= nums[i];
+            ans.push_back(prod);
+        }
+        
+        prod = 1;
+        for(int i=n-1; i>0; i--) {
+            ans[i] = ans[i-1] * prod;
+            prod *= nums[i];
+        }
+        
+        ans[0] = prod;
+        return ans;
+    }
+};
